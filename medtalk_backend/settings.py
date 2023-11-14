c0f798a04b3e28269cbbf2dcf334ac41581a1e13
unknown <neethadhiya@gmail.com>
2023-11-14 22:34:00 +0530

    static deleted

--- medtalk_backend/settings.py
+++ medtalk_backend/settings.py
@@ -124,14 +124,14 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR,"static"    
 # ]
-STATICFILES_DIRS = [
-    BASE_DIR / "static"  # or any other directories
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"  # or any other directories
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
