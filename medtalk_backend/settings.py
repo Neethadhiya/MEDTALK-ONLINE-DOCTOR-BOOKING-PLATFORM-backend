370536ae55583efc7c710231efa03623ac6286b2
unknown <neethadhiya@gmail.com>
2023-11-14 22:15:58 +0530

    static added

--- medtalk_backend/settings.py
+++ medtalk_backend/settings.py
@@ -125,10 +125,14 @@ DATABASES = {
     }
 }
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR,"static"    
+# ]
 STATICFILES_DIRS = [
-    BASE_DIR,"static"    
+    BASE_DIR / "static"  # or any other directories
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
